3719bdb2f969cec4238bda15803bd19ff0a778d3
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2023-07-08 23:55:33 +0300

    main resource done with .env, mongoose model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ExpensesModule } from './expenses/expenses.module';
+import { ConfigModule } from './config/config.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from './config/config.service';
+import { ExpensesModule } from './modules/expenses/expenses.module';
 
 @Module({
-  imports: [ExpensesModule],
+  imports: [
+    ConfigModule,
+    ExpensesModule,
+    // MongoDB Connection
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.getMongoConfig(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
