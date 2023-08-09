5686c0e784fac0142c6b858f3ff2633041f6995f
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-08-09 23:25:49 +0300

    user service updaetd

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -3,8 +3,9 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
+import { ObjectId } from 'bson';
 
-// import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -12,20 +13,42 @@ export class UsersService {
     @InjectModel(User.name) private readonly userModel: Model<User>,
   ) {}
   async create(createUserDto: CreateUserDto) {
-    return await this.userModel.create(createUserDto);
+    // return await this.userModel.create(createUserDto);
+    const isExists = await this.userModel
+      .findOne({
+        email: createUserDto.email,
+        name: createUserDto.name,
+      })
+      .lean();
+
+    if (isExists) {
+      return null;
+    } else {
+      return this.userModel.create(createUserDto);
+    }
   }
 
   async findAll() {
     return await this.userModel.find().exec();
   }
 
-  async findOne(id: number) {
+  async findOne(id: string) {
     return await this.userModel.findOne({ _id: id }).exec();
   }
 
-  // update(id: number, updateUserDto: UpdateUserDto) {
-  //   return `This action updates a #${id} user`;
-  // }
+  async findOneAndUpdate(
+    id: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    // return `This action updates a #${id} user`;
+    updateUserDto['updatedAt'] = Date.now().toString();
+
+    return this.userModel.findOneAndUpdate(
+      { _id: new ObjectId(id) },
+      { $set: updateUserDto },
+      { returnDocument: 'after' },
+    );
+  }
 
   async delete(id: string) {
     return await this.userModel.findByIdAndRemove({ _id: id }).exec();
